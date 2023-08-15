d56c1b4abe82acbc5412b1c6eed706517e9935c6
guidodf98 <guido.di.fiore@gmail.com>
2023-08-14 21:20:27 -0300

    rutas de api

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\CategoriaController;
+use App\Http\Controllers\Api\CursoController;
+use App\Http\Controllers\Api\PersonaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,5 +18,29 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+  return $request->user();
+});
+
+Route::controller(PersonaController::class)->group(function () {
+  Route::get('/personas', 'index');
+  Route::post('/persona', 'store');
+  Route::get('/persona/{id}', 'show');
+  Route::put('/persona/{id}', 'update');
+  Route::delete('/persona/{id}', 'destroy');
+});
+
+Route::controller(CursoController::class)->group(function () {
+  Route::get('/cursos', 'index');
+  Route::post('/curso', 'store');
+  Route::get('/curso/{id}', 'show');
+  Route::put('/curso/{id}', 'update');
+  Route::delete('/curso/{id}', 'destroy');
+});
+
+Route::controller(CategoriaController::class)->group(function () {
+  Route::get('/categorias', 'index');
+  Route::post('/categoria', 'store');
+  Route::get('/categoria/{id}', 'show');
+  Route::put('/categoria/{id}', 'update');
+  Route::delete('/categoria/{id}', 'destroy');
 });
