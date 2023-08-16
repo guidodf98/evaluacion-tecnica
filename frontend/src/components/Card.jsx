782bd41fd76d3a8d029d567a1f5b2da35a53a672
guidodf98 <guido.di.fiore@gmail.com>
2023-08-16 00:15:40 -0300

    baja de cursos personas y categorias

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -4,9 +4,7 @@ const Card = ({ curso }) => {
   return (
     <div className="col">
       <div className="card shadow-sm h-100">
-        <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
-          <rect width="100%" height="100%" fill="#55595c" />
-        </svg>
+        <img className="bd-placeholder-img card-img-top" width="100%" height="200" src="https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/media/image/2022/09/curso-programacion-udemy-2829849.jpg?tf=3840x" alt="cursos de programacion" />
         <div className="card-body d-flex flex-column justify-content-between">
           <div>
             <p className='fs-4 mb-2'>{curso.nombre}</p>
