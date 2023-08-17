702b3f91e1e187f846c9cd2187c652d508ca7267
guidodf98 <guido.di.fiore@gmail.com>
2023-08-17 13:46:52 -0300

    Revert "seccion medios de pago con el mismo formato que servicios"
    This reverts commit 612eae645bb79ab20a0e65e1a5c7e9ab8a9dd0c9.

--- frontend/src/components/Galeria.jsx
+++ frontend/src/components/Galeria.jsx
@@ -5,7 +5,6 @@ import Card from './Card'
 import AnotarPersonas from './Modales/AnotarPersonas'
 
 const endpoint = 'http://localhost:8000/api'
-const endpointFiltrarCurso = 'http://localhost:8000/api/cursos/categoria'
 
 const Galeria = () => {
   const [idCursoSeleccionado, setIdCursoSeleccionado] = useState(null);
@@ -26,20 +25,9 @@ const Galeria = () => {
       });
   }
 
-  const cursosFiltradoCategoria = async (categoriaId) => {
-    await axios.get(`${endpointFiltrarCurso}/${categoriaId}`)
-      .then(response => {
-        console.log(response.data);
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error);
-      });
-  }
-
   return (
     <div className="album py-5 bg-body-tertiary">
       <AnotarPersonas cursoId={idCursoSeleccionado} />
-      <button onClick={() => cursosFiltradoCategoria(1)}>filtrado</button>
       <div className="container">
         <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 g-md-3 g-xl-4">
           {cursos && cursos.length > 0 ? (
